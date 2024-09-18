c52ec1e3a12fd9dbebbe2119e5d3efd88444a49c
PaoDerass <paola_deras@unitec.edu>
2024-09-17 21:01:55 -0600

    creacion vista modelo

---
+++ ViewModel/CirculoViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+
+namespace AreaCirculoAppMvvm.ViewModels
+{
+    public partial class CirculoViewModel : ObservableObject
+    {
+        public const double PI = 3.1415926535897931;
+
+        [ObservableProperty]
+        private double radio;
+
+        [ObservableProperty]
+        private double res;
+
+        [RelayCommand]
+        public void CalcArea()
+        {
+            Res= PI * Radio * Radio;
+        }
+
+        [RelayCommand]
+        public void Limpiar()
+        {
+            Radio = 0;
+            Res = 0;
+        }
+    }
+}
